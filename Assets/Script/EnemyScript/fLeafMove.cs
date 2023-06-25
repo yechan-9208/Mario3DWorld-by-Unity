1daee1f87f4966cb82db071794e6b7cf9ffc31bb
alta521 <alta521521@gmail.com>
2023-06-26 01:40:33 +0900

    .
    .

--- Assets/Script/EnemyScript/fLeafMove.cs
+++ Assets/Script/EnemyScript/fLeafMove.cs
@@ -6,19 +6,25 @@ public class fLeafMove : MonoBehaviour
 {
     public GameObject flowerLeafR;
     public Transform[] leafR;
+    public GameObject obj1, obj2, obj3;
+    
     [Range(0,1)]
     public float valuef;
     // Start is called before the first frame update
     void Start()
     {
-        
+        iTween.MoveBy(gameObject, iTween.Hash("x", 10, "y", 10, "z", 10, "easyType", iTween.EaseType.linear));
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        iTween.PutOnPath(flowerLeafR, leafR, valuef);
-        Destroy(flowerLeafR);
+        
+        //iTween.PutOnPath(flowerLeafR, leafR, valuef);
+        //Destroy(flowerLeafR);
     }
     private void OnDrawGizmos()
     {
